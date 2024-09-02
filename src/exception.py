5415abf3ee1cb8d3aa23014d8f91f34f6016a1f3
Akash H N <akashhn06@gmail.com>
2024-09-03 01:16:08 +0530

    Data Tranformation dane

--- src/exception.py
+++ src/exception.py
@@ -2,6 +2,7 @@ import sys
 import src
 from src.logger import logging
 
+
 def error_message_detail(error, error_details:sys):
     _,_,exc_tb = error_details
     file_name = exc_tb.tb_frame.f_code.co_filename
@@ -10,10 +11,12 @@ def error_message_detail(error, error_details:sys):
 
     return error_message
 
+
 class CustomException(Exception):
     def __init__(self, error_message, error_details:sys):
         super().__init__(error_message)
         self.error_message = error_message_detail(error_message, error_details = error_details)
+
 
 
     def __str__(self):
